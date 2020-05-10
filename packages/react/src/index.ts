595e106c8cd5d7126d9518e25d6660cde5faf47e
Jeroen Claassens <support@favware.tech>
2020-05-10 21:45:52 +0200

    chore: build cleanup

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,20 +1,16 @@
 /* eslint-disable */
 /* tslint:disable */
 /* auto-generated react proxies */
+import { JSX } from '@skyra/discord-components-core';
+import { applyPolyfills, defineCustomElements } from '@skyra/discord-components-core/loader';
 import { createReactComponent } from './react-component-lib';
 
-import { JSX } from '@skyra/discord-components-core';
 
-import { defineCustomElements, applyPolyfills } from '@skyra/discord-components-core/loader';
 
 applyPolyfills().then(() => defineCustomElements());
-export const DiscordEmbed = /*@__PURE__*/ createReactComponent<JSX.DiscordEmbed, HTMLDiscordEmbedElement>('discord-embed');
-export const DiscordEmbedField = /*@__PURE__*/ createReactComponent<JSX.DiscordEmbedField, HTMLDiscordEmbedFieldElement>(
-	'discord-embed-field'
-);
-export const DiscordEmbedFields = /*@__PURE__*/ createReactComponent<JSX.DiscordEmbedFields, HTMLDiscordEmbedFieldsElement>(
-	'discord-embed-fields'
-);
-export const DiscordMention = /*@__PURE__*/ createReactComponent<JSX.DiscordMention, HTMLDiscordMentionElement>('discord-mention');
-export const DiscordMessage = /*@__PURE__*/ createReactComponent<JSX.DiscordMessage, HTMLDiscordMessageElement>('discord-message');
-export const DiscordMessages = /*@__PURE__*/ createReactComponent<JSX.DiscordMessages, HTMLDiscordMessagesElement>('discord-messages');
+export const DiscordEmbed = /*@__PURE__*/createReactComponent<JSX.DiscordEmbed, HTMLDiscordEmbedElement>('discord-embed');
+export const DiscordEmbedField = /*@__PURE__*/createReactComponent<JSX.DiscordEmbedField, HTMLDiscordEmbedFieldElement>('discord-embed-field');
+export const DiscordEmbedFields = /*@__PURE__*/createReactComponent<JSX.DiscordEmbedFields, HTMLDiscordEmbedFieldsElement>('discord-embed-fields');
+export const DiscordMention = /*@__PURE__*/createReactComponent<JSX.DiscordMention, HTMLDiscordMentionElement>('discord-mention');
+export const DiscordMessage = /*@__PURE__*/createReactComponent<JSX.DiscordMessage, HTMLDiscordMessageElement>('discord-message');
+export const DiscordMessages = /*@__PURE__*/createReactComponent<JSX.DiscordMessages, HTMLDiscordMessagesElement>('discord-messages');
