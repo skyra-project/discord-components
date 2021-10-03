946652bb6166e1cf06c681e3fb410ef92975914b
Jeroen Claassens <support@favware.tech>
2021-10-03 14:52:46 +0200

    fix: re-fix react bundle. The output file _has_ to be mjs for this to all work

--- packages/react/scripts/build.mjs
+++ packages/react/scripts/build.mjs
@@ -13,7 +13,7 @@ const result = await esbuild.build({
 	bundle: true,
 	resolveExtensions: ['.ts', '.tsx'],
 	write: true,
-	outfile: fileURLToPath(new URL('index.js', distFolder)),
+	outfile: fileURLToPath(new URL('index.mjs', distFolder)),
 	platform: 'browser',
 	tsconfig: join(fileURLToPath(rootFolder), 'tsconfig.json'),
 	sourcemap: true,
