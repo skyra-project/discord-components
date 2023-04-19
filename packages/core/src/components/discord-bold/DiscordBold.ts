5511da950678947760c0c3a028e31bd385a64669
Jeroen Claassens <support@favware.tech>
2023-04-19 22:47:23 +0200

    feat: migrate discord bold

---
+++ packages/core/src/components/discord-bold/DiscordBold.ts
@@ -0,0 +1,13 @@
+import { html, LitElement } from 'lit';
+import { customElement } from 'lit/decorators.js';
+
+@customElement('discord-bold')
+export class DiscordBold extends LitElement {
+	protected override render() {
+		return html`
+			<strong>
+				<slot></slot>
+			</strong>
+		`;
+	}
+}
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,7 @@
 import type { DiscordMessageOptions } from './options';
 
 export { DiscordAuthorInfo } from './components/discord-author-info/DiscordAuthorInfo.js';
+export { DiscordBold } from './components/discord-bold/DiscordBold.js';
 export { DiscordMention } from './components/discord-mention/DiscordMention.js';
 export { DiscordMessage } from './components/discord-message/DiscordMessage.js';
 export { DiscordMessages } from './components/discord-messages/DiscordMessages.js';
