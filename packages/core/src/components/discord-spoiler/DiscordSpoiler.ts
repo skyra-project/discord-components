6db928b25de380d4cb82c8066b0f727bebcb5031
Jeroen Claassens <support@favware.tech>
2023-05-07 10:20:21 +0200

    fix: fixed discord-spoiler extending the full width instead of just the length of the text

--- packages/core/src/components/discord-spoiler/DiscordSpoiler.ts
+++ packages/core/src/components/discord-spoiler/DiscordSpoiler.ts
@@ -4,38 +4,36 @@ import { customElement } from 'lit/decorators.js';
 @customElement('discord-spoiler')
 export class DiscordSpoiler extends LitElement {
 	public static override styles = css`
-		.discord-spoiler {
+		:host {
 			background-color: #202225;
 			color: transparent;
 			cursor: pointer;
 		}
 
-		.discord-light-theme .discord-message .discord-message-body .discord-spoiler {
+		.discord-light-theme .discord-message .discord-message-body :host {
 			background-color: #b9bbbe;
 		}
 
-		.discord-spoiler:hover {
+		:host(:hover) {
 			background-color: rgba(32, 34, 37, 0.8);
 		}
 
-		.discord-light-theme .discord-message .discord-message-body .discord-spoiler:hover {
+		.discord-light-theme .discord-message .discord-message-body :host(:hover) {
 			background-color: rgba(185, 187, 190, 0.8);
 		}
 
-		.discord-spoiler:active {
+		:host(:active) {
 			color: inherit;
 			background-color: hsla(0, 0%, 100%, 0.1);
 		}
 
-		.discord-light-theme .discord-message .discord-message-body .discord-spoiler:active {
+		.discord-light-theme .discord-message .discord-message-body :host(:active) {
 			background-color: rgba(0, 0, 0, 0.1);
 		}
 	`;
 
 	protected override render() {
-		return html` <div class="discord-spoiler">
-			<slot></slot>
-		</div>`;
+		return html`<slot></slot>`;
 	}
 }
 
