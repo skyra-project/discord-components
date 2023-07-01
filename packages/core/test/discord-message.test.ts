5b532e9474ad18a39c7e484564611c08dbb574be
Jeroen Claassens <support@favware.tech>
2023-07-01 13:08:42 +0200

    test: finish up tests for discord-message

--- packages/core/test/discord-message.test.ts
+++ packages/core/test/discord-message.test.ts
@@ -105,7 +105,7 @@ describe('DiscordMessages', () => {
 	it('does not show bot icon when bot and server options are enabled', async () => {
 		const el = await fixture<DiscordMessages>(html`
 			<discord-messages>
-				<discord-message bot server>Try not. Do or do not. There is no try</discord-message>
+				<discord-message bot server>Who's the more foolish: the fool or the fool who follows him?</discord-message>
 			</discord-messages>
 		`);
 
@@ -118,7 +118,7 @@ describe('DiscordMessages', () => {
 	it('shows server icon when server option is enabled', async () => {
 		const el = await fixture<DiscordMessages>(html`
 			<discord-messages>
-				<discord-message server>Try not. Do or do not. There is no try</discord-message>
+				<discord-message server>No longer certain that one ever does win a war, I am</discord-message>
 			</discord-messages>
 		`);
 
@@ -135,7 +135,7 @@ describe('DiscordMessages', () => {
 	it('shows op icon when OP option is enabled', async () => {
 		const el = await fixture<DiscordMessages>(html`
 			<discord-messages>
-				<discord-message op>Try not. Do or do not. There is no try</discord-message>
+				<discord-message op>Difficult to see; always in motion is the future</discord-message>
 			</discord-messages>
 		`);
 
@@ -152,7 +152,7 @@ describe('DiscordMessages', () => {
 	it('shows bot and op icon when bot and op options are enabled', async () => {
 		const el = await fixture<DiscordMessages>(html`
 			<discord-messages>
-				<discord-message bot op>Try not. Do or do not. There is no try</discord-message>
+				<discord-message bot op>Great, kid, don't get cocky</discord-message>
 			</discord-messages>
 		`);
 
@@ -167,8 +167,97 @@ describe('DiscordMessages', () => {
 			?.shadowRoot?.querySelector('.discord-application-tag-op')
 			?.textContent?.trim();
 
+		expect(discordMessage.bot).to.equal(true);
+		expect(discordMessage.op).to.equal(true);
+
 		expect(applicationTag).to.equal('Bot');
 		expect(applicationOpTag).to.equal('OP');
-		expect(discordMessage.op).to.equal(true);
+	});
+
+	it('shows bot and verified icon when bot and verified options are enabled', async () => {
+		const el = await fixture<DiscordMessages>(html`
+			<discord-messages>
+				<discord-message bot verified>The Force is strong with this one</discord-message>
+			</discord-messages>
+		`);
+
+		const discordMessage = getNotNullFirstChildElement<DiscordMessage>(el);
+		const applicationTag = discordMessage.shadowRoot
+			?.querySelector('discord-author-info')
+			?.shadowRoot?.querySelector('.discord-application-tag')
+			?.textContent?.trim();
+		const verifiedTag = discordMessage.shadowRoot
+			?.querySelector('discord-author-info')
+			?.shadowRoot?.querySelector('.discord-application-tag')?.firstElementChild;
+
+		expect(discordMessage.bot).to.equal(true);
+		expect(discordMessage.verified).to.equal(true);
+
+		expect(applicationTag).to.equal('Bot');
+		expect(verifiedTag).to.instanceOf(SVGElement);
+		expect(verifiedTag?.classList.contains('discord-application-tag-verified')).to.equal(true);
+	});
+
+	it('shows highlight styling when the option is enabled', async () => {
+		const el = await fixture<DiscordMessages>(html`
+			<discord-messages>
+				<discord-message highlight>Sorry lady. I don't understand frog</discord-message>
+			</discord-messages>
+		`);
+
+		const discordMessage = getNotNullFirstChildElement<DiscordMessage>(el);
+		expect(discordMessage.highlight).to.equal(true);
+		expect(window.getComputedStyle(discordMessage).backgroundColor).to.equal('rgba(250, 166, 26, 0.1)');
+	});
+
+	it('shows edited tag when the option is enabled', async () => {
+		const el = await fixture<DiscordMessages>(html`
+			<discord-messages>
+				<discord-message edited>I'm a Mandalorian. Weapons are part of my religion</discord-message>
+			</discord-messages>
+		`);
+
+		const discordMessage = getNotNullFirstChildElement<DiscordMessage>(el);
+		const editedContent = discordMessage.shadowRoot?.querySelector('.discord-message-edited')?.textContent;
+
+		expect(discordMessage.edited).to.equal(true);
+		expect(editedContent).to.equal('(edited)');
+	});
+
+	it('shows ephemeral tag when the option is enabled', async () => {
+		const el = await fixture<DiscordMessages>(html`
+			<discord-messages>
+				<discord-message ephemeral bot>This is the way</discord-message>
+			</discord-messages>
+		`);
+
+		const discordMessage = getNotNullFirstChildElement<DiscordMessage>(el);
+		const ephemeralContent = discordMessage.shadowRoot?.querySelector('.discord-message-ephemeral')?.childNodes[3].textContent?.trim();
+
+		expect(discordMessage.ephemeral).to.equal(true);
+		expect(ephemeralContent).to.equal('Only you can see this •');
+	});
+
+	it('shows role icon the option is enabled', async () => {
+		const el = await fixture<DiscordMessages>(html`
+			<discord-messages>
+				<discord-message
+					author="Favna"
+					role-name="Admin"
+					role-color="#20505f"
+					avatar="https://cdn.skyra.pw/gh-assets/sapphire.png"
+					role-icon="https://cdn.skyra.pw/gh-assets/sapphire.png"
+				>
+					I find your lack of faith disturbing
+				</discord-message>
+			</discord-messages>
+		`);
+
+		const discordMessage = getNotNullFirstChildElement<DiscordMessage>(el);
+
+		expect(discordMessage.author).to.equal('Favna');
+		expect(discordMessage.roleName).to.equal('Admin');
+		expect(discordMessage.roleColor).to.equal('#20505f');
+		expect(discordMessage.roleIcon).to.equal('https://cdn.skyra.pw/gh-assets/sapphire.png');
 	});
 });
