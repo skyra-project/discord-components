f11f11284fc328f94a4e2a06e325cd7886eee5d8
Jeroen Claassens <support@favware.tech>
2024-07-13 13:26:14 +0200

    build(core): esbuild + hmr for dev server

--- packages/core/web-dev-server.config.mjs
+++ packages/core/web-dev-server.config.mjs
@@ -1,5 +1,17 @@
+import { fileURLToPath, URL } from 'node:url';
+import { esbuildPlugin } from '@web/dev-server-esbuild';
+
 export default /** @type {import('@web/dev-server').DevServerConfig} */ ({
+	plugins: [
+		esbuildPlugin({
+			ts: true,
+			target: 'es2020',
+			tsconfig: fileURLToPath(new URL('src/tsconfig.json', import.meta.url))
+		})
+	],
+
 	open: '/demo/',
+	watch: true,
 
 	nodeResolve: {
 		exportConditions: ['browser', 'development']
