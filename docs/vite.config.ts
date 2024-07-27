950ffbccd4dfce61baf611fcb88051fd2c193e57
Jeroen Claassens <support@favware.tech>
2024-07-27 03:00:40 +0200

    chore: remove unnecessary base configuration in vite.config.ts

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,9 +1,3 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-	// TODO(CNAME): Remove when we get discord-components.js.org
-	base: '/discord-components/'
-});
-
-// TODO(CNAME): Run once when we get discord-components.js.org
-// printf "discord-components.js.org" >> docs/public/CNAME
+export default defineConfig({});
