8f789dc0004d39f5b13481d4e5ec021d67c1a35f
Marciel404 <48138111+eumarciel404@users.noreply.github.com>
2024-07-28 20:04:20 -0300

    fix: Removing Useless Import
    Accidentally I imported button class in action-row file

--- packages/core/src/components/discord-action-row/DiscordActionRow.ts
+++ packages/core/src/components/discord-action-row/DiscordActionRow.ts
@@ -1,6 +1,5 @@
 import { css, html, LitElement } from 'lit';
 import { customElement } from 'lit/decorators.js';
-import { DiscordButton } from '../discord-button/DiscordButton.js';
 
 @customElement('discord-action-row')
 export class DiscordActionRow extends LitElement {
