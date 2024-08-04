e8e6e6d3946a47640b5c9d93f6718462466587ee
Marciel404 <48138111+eumarciel404@users.noreply.github.com>
2024-08-03 22:58:58 -0300

    Adding light theme and correcting text ellipsis
    Before, there was no clear theme and the text of the description and label of the option was larger than the body of the div

--- packages/core/src/components/discord-string-select-menu-option/DiscordStringSelectMenuOption.ts
+++ packages/core/src/components/discord-string-select-menu-option/DiscordStringSelectMenuOption.ts
@@ -3,6 +3,8 @@ import { customElement, property } from 'lit/decorators.js';
 import { ifDefined } from 'lit/directives/if-defined.js';
 import { when } from 'lit/directives/when.js';
 import { DiscordComponentsError } from '../../util.js';
+import { consume } from '@lit/context';
+import { messagesLightTheme } from '../discord-messages/DiscordMessages.js';
 
 @customElement('discord-string-select-menu-option')
 export class DiscordStringSelectMenuOption extends LitElement {
@@ -17,11 +19,20 @@ export class DiscordStringSelectMenuOption extends LitElement {
 			padding: 8px 8px 8px 12px;
 			gap: 10px;
 			font-size: small;
-			max-width: 95%;
 		}
 
 		:host(:hover) {
-			background-color: rgba(0, 0, 0, 0.1);
+			background-color: rgba(255, 255, 255, 0.1);
+		}
+
+		:host([light-theme]) {
+			background-color: #F2F3F5 !important;
+			border-color: #D9D9D9 !important;
+			color: #2e3338;
+		}
+
+		:host([light-theme]:hover) {
+			background-color: rgba(204, 204, 204, 2) !important;
 		}
 
 		.discord-string-select-menu-option-emoji {
@@ -32,12 +43,10 @@ export class DiscordStringSelectMenuOption extends LitElement {
 			vertical-align: bottom;
 		}
 
-		.discord-string-select-menu-option-max-size {
-			display: block;
+		.discord-string-select-menu-option-ellipsis-text {
 			overflow: hidden;
 			text-overflow: ellipsis;
 			white-space: nowrap;
-			max-width: 390px;
 		}
 	`;
 
@@ -71,6 +80,10 @@ export class DiscordStringSelectMenuOption extends LitElement {
 		}
 	}
 
+	@consume({ context: messagesLightTheme })
+	@property({ type: Boolean, reflect: true, attribute: 'light-theme' })
+	public accessor lightTheme = false;
+
 	protected override render() {
 		this.checkLabelIsProvided();
 
@@ -80,11 +93,11 @@ export class DiscordStringSelectMenuOption extends LitElement {
 				() =>
 					html`<img src=${this.emoji} alt=${ifDefined(this.emojiName)} draggable="true" class="discord-string-select-menu-option-emoji" />`
 			)}
-			<div>
-				<div>
-					<strong class="discord-string-select-menu-option-max-size">${this.label}</strong>
+			<div class="discord-string-select-menu-option-ellipsis-text">
+				<div class="discord-string-select-menu-option-ellipsis-text">
+					<strong>${this.label}</strong>
 				</div>
-				${when(this.description, () => html`<span class="discord-string-select-menu-option-max-size">${this.description}</span>`)}
+				${when(this.description, () => html`<span>${this.description}</span>`)}
 			</div>
 		`;
 	}
