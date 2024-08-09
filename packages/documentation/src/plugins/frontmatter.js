fe237595f8af20a11d9d38e872ee1ee1af61b35f
Jeroen Claassens <support@favware.tech>
2024-08-09 21:02:40 +0200

    docs: update frontmatter plugin config

--- packages/documentation/src/plugins/frontmatter.js
+++ packages/documentation/src/plugins/frontmatter.js
@@ -10,15 +10,6 @@ export function load(app) {
 		 * @param {MarkdownPageEvent} page
 		 */
 		(page) => {
-			page.frontmatter = {
-				title: page.model.name,
-				editUrl: false,
-				next: true,
-				prev: true,
-
-				...page.frontmatter
-			};
-
 			if (page.url === 'README.md') {
 				page.frontmatter.tableOfContents = {
 					minHeadingLevel: 1,
