629bbea0b8b416e9f3366623adeb616905e50fb9
Jeroen Claassens <support@favware.tech>
2024-08-10 23:07:14 +0200

    fix: bold and italic in list items proper indenting (#445)
    fixes #444

--- packages/core/src/components/discord-bold/DiscordBold.ts
+++ packages/core/src/components/discord-bold/DiscordBold.ts
@@ -1,8 +1,17 @@
-import { LitElement, html } from 'lit';
+import { LitElement, css, html } from 'lit';
 import { customElement } from 'lit/decorators.js';
 
 @customElement('discord-bold')
 export class DiscordBold extends LitElement {
+	/**
+	 * @internal
+	 */
+	public static override readonly styles = css`
+		:host > strong {
+			font-weight: 700;
+		}
+	`;
+
 	protected override render() {
 		return html`
 			<strong>
