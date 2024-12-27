9397a3b4e9c8d9bd54de461971aae291cc6b634e
TheBv <bv939@web.de>
2024-12-27 17:42:35 +0100

    feat: added option to add the channel name above discord messages

--- packages/core/src/components/discord-messages/DiscordMessages.ts
+++ packages/core/src/components/discord-messages/DiscordMessages.ts
@@ -3,6 +3,11 @@ import { css, html, LitElement } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { defaultBackground, defaultMode, defaultTheme } from '../../config.js';
 import type { LightTheme } from '../../types.js';
+import ChannelForum from '../svgs/ChannelForum.js';
+import ChannelIcon from '../svgs/ChannelIcon.js';
+import ChannelThread from '../svgs/ChannelThread.js';
+import LockedVoiceChannel from '../svgs/LockedVoiceChannel.js';
+import VoiceChannel from '../svgs/VoiceChannel.js';
 
 export const messagesLightTheme = createContext<boolean>('light-theme');
 export const messagesCompactMode = createContext<boolean>('compact-mode');
@@ -55,6 +60,35 @@ export class DiscordMessages extends LitElement implements LightTheme {
 			margin-bottom: 0.5rem;
 			border-bottom-width: 0;
 		}
+
+		:host .discord-channel-header {
+			display: flex;
+			align-items: center;
+			padding: 0.5rem 1rem;
+			box-shadow:
+				0 2px 0 0 rgba(0, 0, 0, 0.05),
+				0 1.5px 0 0 rgba(0, 0, 0, 0.05),
+				0 1px 0 0 rgba(0, 0, 0, 0.16);
+		}
+
+		:host .discord-channel-icon {
+			height: 24px;
+			width: auto;
+			margin: 0 8px;
+			position: relative;
+			flex: 0 0 auto;
+			color: #80848e;
+		}
+
+		:host([light-theme]) .discord-channel-icon {
+			color: #6d6f78;
+		}
+
+		:host .discord-channel-name {
+			margin: 0 8px 0 0;
+			flex: 0 0 auto;
+			min-width: auto;
+		}
 	`;
 
 	/**
@@ -78,6 +112,16 @@ export class DiscordMessages extends LitElement implements LightTheme {
 	@property({ type: Boolean, reflect: true, attribute: 'compact-mode' })
 	public accessor compactMode = false;
 
+	/**
+	 * The type of mention this should be. This will prepend the proper prefix character.
+	 * Valid values: `user`, `channel`, `role`, `voice`, `locked`, `thread`, `forum`, and `slash`.
+	 */
+	@property({ reflect: true, attribute: 'type' })
+	public accessor type: 'channel' | 'forum' | 'locked' | 'thread' | 'voice';
+
+	@property({ reflect: true, attribute: 'channel-name' })
+	public accessor name: string;
+
 	public override connectedCallback(): void {
 		super.connectedCallback();
 
@@ -95,7 +139,34 @@ export class DiscordMessages extends LitElement implements LightTheme {
 	}
 
 	protected override render() {
-		return html`<slot></slot>`;
+		let channelIcon: ReturnType<typeof html>;
+		switch (this.type) {
+			case 'channel':
+				channelIcon = html`${ChannelIcon()}`;
+				break;
+			case 'voice':
+				channelIcon = html`${VoiceChannel()}`;
+				break;
+			case 'locked':
+				channelIcon = html`${LockedVoiceChannel()}`;
+				break;
+			case 'thread':
+				channelIcon = html`${ChannelThread()}`;
+				break;
+			case 'forum':
+				channelIcon = html`${ChannelForum()}`;
+				break;
+		}
+
+		return html`<div>
+			${this.type && this.name
+				? html`<div class="discord-channel-header">
+						<div class="discord-channel-icon">${channelIcon}</div>
+						<div class="discord-channel-name">${this.name}</div>
+					</div>`
+				: ''}
+			<slot></slot>
+		</div>`;
 	}
 }
 
